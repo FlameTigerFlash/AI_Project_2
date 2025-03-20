652481351a41314e35b964403c16a1db19770a6b
Matvey <69459474+FlameTigerFlash@users.noreply.github.com>
2025-03-20 13:48:17 +0300

    Add files via upload

--- RAG/rag.py
+++ RAG/rag.py
@@ -26,6 +26,59 @@ def filter_name(s:str):
   return ans
 
 
+def preprocess_text(text:str):
+    while "\n\n\n\n" in text:
+        text = text.replace("\n\n\n\n", "\n\n")
+
+    chunks = text.split("\n\n")
+    arr = [0 for i in range(len(chunks))]
+    for i in range(len(chunks) - 1):
+        if chunks[i][0].isdigit() and chunks[i+1][0].isdigit():
+            arr[i] = 1
+            continue
+        max_pref = 0
+        for j in range(min(len(chunks[i]), len(chunks[i+1]))):
+            if chunks[i][j] != chunks[i+1][j]:
+                break
+            max_pref += 1
+        if max_pref >= 3:
+            arr[i] = 1
+
+    new_chunks = []
+    s = ""
+    for i in range(len(chunks)):
+        if arr[i] == 1:
+            s += "\n" * (len(s) > 0) + chunks[i]
+        else:
+            s += "\n" * (len(s) > 0) + chunks[i]
+            new_chunks.append(s)
+            s = ""
+        if i == len(chunks) - 1 and arr[i] == 1:
+            new_chunks.append(s)
+
+    chunks = new_chunks.copy()
+    new_chunks.clear()
+    arr = [0 for i in range(len(chunks))]
+
+    for i in range(len(chunks) - 1):
+        if chunks[i][0].isdigit():
+            continue
+        if (len(chunks[i]) < 60 and len(chunks[i+1]) >= 2 * len(chunks[i])) or (len(chunks[i]) < 100 and len(chunks[i+1]) >= 4 * len(chunks[i])):
+            arr[i] = 1
+
+    s = ""
+    for i in range(len(chunks)):
+        if arr[i] == 1:
+            s += "\n" * (len(s) > 0) + chunks[i]
+        else:
+            s += "\n" * (len(s) > 0) + chunks[i]
+            new_chunks.append(s)
+            s = ""
+        if i == len(chunks) - 1 and arr[i] == 1:
+            new_chunks.append(s)
+    return "\n\n".join(new_chunks)
+
+
 class RAG:
     def __init__(self, embeddings: HuggingFaceEmbeddings, path:str):
         self.embeddings = embeddings
@@ -40,7 +93,7 @@ class RAG:
             is_separator_regex=False,
         )
 
-    def vectorize_one(self, addr="dataset", file="burns.txt"):
+    def vectorize_one(self, addr="dataset", file="burns.txt", preprocess:bool = False):
         full_path = addr+"/"+file
         if not os.path.exists(full_path):
             print(f"Файла не существует: {full_path}")
@@ -50,6 +103,14 @@ class RAG:
         text_loader = TextLoader(full_path, encoding='utf-8')
         documents = text_loader.load()
         text_contents = [doc.page_content for doc in documents]
+
+        if preprocess:
+            for (i, el) in enumerate(text_contents):
+                try:
+                    text_contents[i] = preprocess_text(el)
+                except Exception as ex:
+                    print(ex.__context__)
+
         split_documents = self.splitter.create_documents(text_contents)
         vector_store = FAISS.from_documents(
             split_documents, self.embeddings
@@ -62,7 +123,7 @@ class RAG:
         vector_store.save_local(f"{self.path}/{new_name}")
         self.location[new_name] = f"/{self.path}/{new_name}"
 
-    def vectorize_all(self, addr="dataset"):
+    def vectorize_all(self, addr="dataset", preprocess:bool = False):
         for root, dirs, files in os.walk(addr):
             for file in files:
                 if file.endswith('.txt') or file.endswith('.TXT'):
@@ -74,6 +135,14 @@ class RAG:
                     text_loader = TextLoader(file_path, encoding='utf-8')
                     documents = text_loader.load()
                     text_contents = [doc.page_content for doc in documents]
+
+                    if preprocess:
+                        for (i, el) in enumerate(text_contents):
+                            try:
+                                text_contents[i] = preprocess_text(el)
+                            except Exception as ex:
+                                print(ex.__context__)
+
                     split_documents = self.splitter.create_documents(text_contents)
                     vector_store = FAISS.from_documents(
                         split_documents, self.embeddings
@@ -95,10 +164,8 @@ class RAG:
             allow_dangerous_deserialization=True
         )
         retriever = vector_store.as_retriever(
-            search_type="mmr",
-            k=k,
-            score_threshold=threshold,
-            lambda_mult=0.6
+            search_type="similarity_score_threshold",
+            search_kwargs={"k": k, "score_threshold": threshold}
         )
         return retriever.invoke(
             question
@@ -112,9 +179,10 @@ if __name__ == "__main__":
         model_kwargs={"device": "cpu"},
         encode_kwargs={"normalize_embeddings": True}
     )
-    rag = RAG(embeddings=embeddings, path='vector_store')
-    rag.vectorize_one(addr='dataset', file='sugar.txt')
 
-    for el in rag.retrieve(filename='sugar', question='На какие пять основных групп делятся продукты?', k=3, threshold=50000):
+    rag = RAG(embeddings=embeddings, path='vector_store')
+    #rag.vectorize_all(addr='dataset', preprocess=True)
+
+    for el in rag.retrieve(filename='sugar', question='Что следует включить в рацион согласно DASH-диете?', k=3, threshold=0.2):
         print(el.page_content)
-        print('\n-----\n-----')
+        print('-----\n-----')
