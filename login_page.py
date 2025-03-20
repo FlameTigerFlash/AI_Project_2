769c3c069c7491ac5e1e3ceadf8bf2adf860bdd7
evgenii420 <eugeneaziattsev@yahoo.com>
2025-03-20 16:33:49 +0300

    Update login_page.py

--- login_page.py
+++ login_page.py
@@ -1,22 +1,15 @@
 import streamlit as st
 
-st.title("Добро пожаловать в бота медицинской помощи!")
-    
-# Создание макета страницы
-col1, col2 = st.columns([2, 1])
-    
-with col1:
-    # Поля ввода логина и пароля, кнопка вход
+def registration_page():
+    st.title("Регистрация в боте медицинской помощи")
+        
+    # Поля для регистрации
+    first_name = st.text_input("Имя")
+    last_name = st.text_input("Фамилия")
+    middle_name = st.text_input("Отчество")
+    age = st.number_input("Возраст", min_value=0, max_value=120, step=1)
+    medical_info = st.text_area("Медицинские особенности")
+        
+    # Поля логина и пароля
     login = st.text_input("Логин")
     password = st.text_input("Пароль", type="password")
-
-    if st.button("Вход"):
-        st.success("Попытка входа...")
-    
-with col2:
-    # Кнопки справа
-    if st.button("Регистрация"):
-        st.info("Переход к регистрации...")
-    if st.button("Поддержка"):
-        st.warning("Свяжитесь с поддержкой..."
-            "       tg: @KALBkabiir")
